40b05a4b6dba0352d5332f59b25eefd287edd8d7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2024-11-16 15:06:39 +1000

    wip

--- run.ts
+++ run.ts
@@ -3,28 +3,73 @@ import semver from "semver";
 import { pipeline } from "node:stream/promises";
 import * as tar from "tar";
 
-async function getMaxSatisfies(dep: string, vers: string, depth = 0) {
+interface Dep {
+  name: string;
+  desired: string;
+  max: string | undefined;
+  parent: Dep;
+  deps: Dep[];
+}
+
+const reg = "https://registry.npmjs.org";
+
+async function getMaxSatisfies(
+  dep: string,
+  vers: string,
+  depth = 0
+): Promise<{ max: string; nested: Record<string, string> }> {
   console.log();
   console.log(`${"  ".repeat(depth)}${dep}@${vers}`);
   const meta = await fetchMetadata(dep);
   const versions = Object.keys(meta["versions"]);
   const max = semver.maxSatisfying(versions, vers as string)!;
   console.log(`${"  ".repeat(depth)}Best match => ${max}`);
-  const nestedDeps = meta.versions[max].dependencies ?? {};
+  const nested = meta.versions[max].dependencies ?? {};
 
-  console.log(`${"  ".repeat(depth + 1)}Deps => ${Object.keys(nestedDeps)}`);
+  console.log(`${"  ".repeat(depth + 1)}Deps => ${Object.keys(nested)}`);
 
-  await getDependencies(nestedDeps, depth + 1);
+  return {
+    max,
+    nested,
+  };
+
+  // let all = await getDependencies(nestedDeps, depth + 1, parent);
 }
 
-async function getDependencies(deps: Record<string, string>, depth = 0) {
-  // console.log(deps);
-  for (const [dep, vers] of Object.entries(deps)) {
-    await getMaxSatisfies(dep, vers as string, depth);
+async function getDependencies(
+  deps: Record<string, string>,
+  parent: Dep,
+  depth: number
+): Promise<Dep[]> {
+  for (const [depName, vers] of Object.entries(deps)) {
+    const { max, nested } = await getMaxSatisfies(
+      depName,
+      vers as string,
+      depth
+    );
+
+    const dep: Dep = {
+      name: depName,
+      parent,
+      desired: vers,
+      max,
+      deps: [],
+    };
+    dep.deps = await getDependencies(nested, dep, depth + 1);
+    return dep.deps;
   }
 }
 
-const reg = "https://registry.npmjs.org";
+const pkgmanJson = JSON.parse(await fs.readFile("./pkgman.json", "utf-8"));
+const deps = pkgmanJson.dependencies;
+const root: Omit<Dep, "desired" | "max" | "parent"> = {
+  name: "root",
+  deps: [],
+};
+
+const moduleGraph = await getDependencies(deps, root as Dep, 0);
+
+console.log(moduleGraph);
 
 async function fetchMetadata(lib: string) {
   const res = await globalThis.fetch(`${reg}/${lib}`);
@@ -42,7 +87,3 @@ async function downloadTar(link: string, to: string) {
 const react = await fetchMetadata("react");
 
 // downloadTar(react.versions["18.3.1"]["dist"]["tarball"], "react");
-const pkgmanJson = JSON.parse(await fs.readFile("./pkgman.json", "utf-8"));
-const deps = pkgmanJson.dependencies;
-
-await getDependencies(deps);
